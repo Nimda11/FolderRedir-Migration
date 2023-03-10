2977fca83ece2583b76caca5b1caf0ba90f4a372
Sam Forma <sam@formatech-it.com>
2023-03-10 12:36:04 -0800

    Initial Upload

---
+++ 2_Reset_KFL.bat
@@ -0,0 +1,129 @@
+@echo off
+
+echo This script will reset all standard user profile folders to their default locations. 
+echo Press any key to continue, or Ctrl+C to exit
+PAUSE >nul
+CLS
+GOTO CONFIRM_UAC
+
+:CONFIRM_UAC
+
+echo This script should NOT be run from an elevated command prompt
+echo Press any key to continue, or Ctrl+C to exit
+PAUSE >nul
+CLS
+GOTO CONFIRM_FolderRedir
+
+:CONFIRM_FolderRedir
+echo This script should be run only after first running the FolderRedirect_Kill Script
+echo Press any key to continue, or Ctrl+C to exit
+PAUSE >nul
+CLS
+GOTO RESETFOLDERS
+
+
+:RESETFOLDERS
+taskkill /f /im explorer.exe >nul
+
+:APPDATA
+SET FOLDER=Appdata\Roaming
+SET REGVAL=Appdata
+call :SF_DEFAULT_FUNCTION
+
+:PICTURES
+SET FOLDER=Pictures
+SET REGVAL=My Pictures
+call :SF_DEFAULT_FUNCTION
+
+:DESKTOP 
+SET FOLDER=Desktop
+SET REGVAL=Desktop
+call :SF_DEFAULT_FUNCTION
+
+:DOCUMENTS
+SET FOLDER=Documents
+SET REGVAL=Personal
+call :SF_DEFAULT_FUNCTION
+
+:MUSIC
+SET FOLDER=Music
+SET REGVAL=My Music
+call :SF_DEFAULT_FUNCTION
+
+:VIDEOS
+SET FOLDER=Videos
+SET REGVAL=My Videos
+call :SF_DEFAULT_FUNCTION
+
+:FAVORITES
+SET FOLDER=Favorites
+SET REGVAL=Favorites
+call :SF_DEFAULT_FUNCTION
+
+:SAVEDGAMES
+SET FOLDER=Saved Games
+SET REGVAL={4C5C32FF-BB9D-43B0-B5B4-2D72E54EAAA4}
+call :SF_DEFAULT_FUNCTION
+
+:DOWNLOADS
+SET FOLDER=Downloads
+SET REGVAL={374DE290-123F-4565-9164-39C4925E467B}
+call :SF_DEFAULT_FUNCTION
+
+:CONTACTS
+SET FOLDER=Contacts
+SET REGVAL={56784854-C6CB-462B-8169-88E350ACB882}
+call :SF_DEFAULT_FUNCTION
+
+:SEARCHES
+SET FOLDER=Searches
+SET REGVAL={7D1D3A04-DEBB-4115-95CF-2F29DA2920DA}
+call :SF_DEFAULT_FUNCTION
+
+:LINKS
+SET FOLDER=Links
+SET REGVAL={BFB9D5E0-C6A9-404C-B2B2-AE6DB6AF4968}
+call :SF_DEFAULT_FUNCTION
+
+:STARTMENU
+SET FOLDER=AppData\Roaming\Microsoft\Windows\Start Menu
+SET REGVAL=Start Menu
+
+GOTO ONEDRIVE_KFL_RESET
+
+:SF_DEFAULT_FUNCTION
+ECHO Resetting %FOLDER% location to default
+if not exist "%USERPROFILE%\%FOLDER%" mkdir "%USERPROFILE%\%FOLDER%" >nul
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders" /v "%REGVAL%" /t REG_SZ /d "C:\Users\%USERNAME%\%FOLDER%" /f >nul 
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "%REGVAL%" /t REG_EXPAND_SZ /d %%USERPROFILE%%"\%FOLDER%" /f >nul
+attrib +r -s -h "%USERPROFILE%\%FOLDER%" /S /D
+ECHO %FOLDER% location reset complete
+REM PAUSE
+GOTO:EOF
+
+:ONEDRIVE_KFL_RESET
+
+
+powershell -command "Get-ItemPropertyValue -path HKCU:SOFTWARE\Microsoft\OneDrive\ -name OneDriveTrigger"
+reg DELETE HKCU\Software\Microsoft\OneDrive\Accounts\Business1\ /v KfmIsDoneSilentOptIn /F >nul
+reg ADD HKCU\Software\Microsoft\OneDrive\Accounts\Business1\ /v KfmIsDoneSilentOptIn /t REG_DWORD /d 0 /F >nul
+reg DELETE HKCU\Software\Microsoft\OneDrive\Accounts\Business1\ /v KfmSilentAttemptedFolders /F >nul
+reg DELETE HKCU\Software\Microsoft\OneDrive\Accounts\Business1\ /v KfmState /F >nul
+reg DELETE HKCU\Software\Microsoft\OneDrive\Accounts\Business1\ /v KfmFoldersProtectedNow /F >nul
+reg DELETE HKCU\Software\Microsoft\OneDrive\Accounts\Business1\ /v LastKnownCloudFilesEnabled /F >nul
+reg DELETE HKCU\Software\Microsoft\OneDrive\Accounts\Business1\ /v KfmFoldersProtectedOnce /F >nul
+
+START "" explorer.exe >nul
+
+taskkill /f /im onedrive.exe >nul
+gpupdate /force >nul
+set LOGDIR=C:\IT_Admin\SpecialFolders
+if not exist "%LOGDIR%" mkdir "%LOGDIR%" >nul
+powershell.exe -command "$var = Get-ItemPropertyValue -path HKCU:SOFTWARE\Microsoft\OneDrive\ -name OneDriveTrigger ; Set-Content -Path $env:LOGDIR\ODPath.txt  -Value $var" >nul
+set /p fODPATH=<%LOGDIR%\ODPath.txt
+START "" "%fODPATH%"
+
+
+
+ECHO Reset Complete, hit any key to exit
+PAUSE >nul
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Summary
+This is a series of scripts intended to help decomission a "sticky" folderredirection deployment with the end goal of allowing migration to OneDrive Known Folder Migration (KFM)
+The user object should no longer be within the scope of a Folder Redirection policy in active directory prior to running these scripts.
+
+Written in Batch and calls Powershell as needed.
+
+Tested against Windows 10 and 11, onedrive clients that are current as of Q1 of 2023
+Not tested against 32bit OS's or Servers.
+
+# LoginScript.bat
+Should be run as a login script. Very simple purpose, it logs the current paths for the Pictures, Documents, and Desktop folders every time a user logs in. The main point being that a technician can find these paths later in case we need to "recover" user documents. Can be deployed as a scheduled task, or preferable as a login script via the users AD Account or GPO.
+
+# 1_FolderRedir_Kill.bat
+This script removes Folder Redirection related registry entries. This allows known folders paths to be changed, but it does NOT change them (2_Reset_KFL.bat actually changes the paths)
+
+**Because these registry entries are located in the HKLM registry hive, this script must be run with administrative privliges.
+
+This script should only be run on a system when a single user is logged in, so no active terminal servers, or pc's with mulitple people logged in (hotdesks).
+
+The script looks up the SID of the logged in user, and deletes the following registry key
+    HKLM:SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\<SID>\fdeploy\
+
+# 2_Reset_KFL.bat
+This script
+1. resets the paths of all possible redirected folders for the logged in user.
+2. deletes several registry entries related to KFL Migration as sometimes automatic enrollment doesn't work.
+3. Runs a GPUPDATE
+4. Stops and Restarts Explorer and OneDrive
+
